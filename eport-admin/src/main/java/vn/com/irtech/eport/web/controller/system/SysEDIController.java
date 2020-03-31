38a618209f802b3dd0fe321fc9ed272c7b292815
Tran Cong Minh <trancongminh1134@gmail.com>
2020-03-31 08:24:59 +0700

    +update empty noti

--- eport-admin/src/main/java/vn/com/irtech/eport/web/controller/system/SysEDIController.java
+++ eport-admin/src/main/java/vn/com/irtech/eport/web/controller/system/SysEDIController.java
@@ -1,29 +1,17 @@
 package vn.com.irtech.eport.web.controller.system;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Scanner;
-
 import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.alibaba.fastjson.JSONObject;
-import com.google.common.io.Files;
-
 import vn.com.irtech.eport.common.core.controller.BaseController;
 
 
@@ -31,8 +19,6 @@ import vn.com.irtech.eport.common.core.controller.BaseController;
 @RequestMapping("/EDI")
 public class SysEDIController extends BaseController
 {
-    private static final String UPLOADED_FOLDER = null;
-
 	@GetMapping("/index")
     public String test()
     {
@@ -46,9 +32,8 @@ public class SysEDIController extends BaseController
 	        HttpServletRequest request) throws IOException {
     	
 		if (file.isEmpty()) {
-			System.out.println("Đ có");
+			System.out.println("File empty");
 		}
-		
 		try {
 			String fileName = file.getOriginalFilename();
 		      File filenew = new File(this.getFolderUpload(), fileName);
@@ -56,7 +41,8 @@ public class SysEDIController extends BaseController
 		      File myObj = new File(this.getFolderUpload()+"/"+fileName);
 		      Scanner myReader = new Scanner(myObj);
 		      while (myReader.hasNextLine()) {
-		        String data = myReader.nextLine();
+                String data = myReader.nextLine();
+                //Xử lý data ở đây
 		        System.out.println(data);
 		      }
 		      myReader.close();
@@ -66,7 +52,8 @@ public class SysEDIController extends BaseController
 		}
 
 		return getSuccessMessage().toString();
-	}
+    }
+    
     public File getFolderUpload() {
         File folderUpload = new File(System.getProperty("user.home") + "/EDI");
         if (!folderUpload.exists()) {
