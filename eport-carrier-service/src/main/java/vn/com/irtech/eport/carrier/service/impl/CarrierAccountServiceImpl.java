21d1cc492936a4dff35d5f35189e5bcca3b15389
GiapHD <giapmaster@gmail.com>
2020-04-04 11:30:56 +0700

    Change project shipping to carrier

---
+++ eport-carrier-service/src/main/java/vn/com/irtech/eport/carrier/service/impl/CarrierAccountServiceImpl.java
@@ -0,0 +1,97 @@
+package vn.com.irtech.eport.carrier.service.impl;
+
+import java.util.List;
+import vn.com.irtech.eport.common.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import vn.com.irtech.eport.carrier.mapper.CarrierAccountMapper;
+import vn.com.irtech.eport.carrier.domain.CarrierAccount;
+import vn.com.irtech.eport.carrier.service.ICarrierAccountService;
+import vn.com.irtech.eport.common.core.text.Convert;
+
+/**
+ * Carrier AccountService Business Processing
+ * 
+ * @author irtech
+ * @date 2020-04-04
+ */
+@Service
+public class CarrierAccountServiceImpl implements ICarrierAccountService 
+{
+    @Autowired
+    private CarrierAccountMapper carrierAccountMapper;
+
+    /**
+     * Get Carrier Account
+     * 
+     * @param id Carrier AccountID
+     * @return Carrier Account
+     */
+    @Override
+    public CarrierAccount selectCarrierAccountById(Long id)
+    {
+        return carrierAccountMapper.selectCarrierAccountById(id);
+    }
+
+    /**
+     * Get Carrier Account List
+     * 
+     * @param carrierAccount Carrier Account
+     * @return Carrier Account
+     */
+    @Override
+    public List<CarrierAccount> selectCarrierAccountList(CarrierAccount carrierAccount)
+    {
+        return carrierAccountMapper.selectCarrierAccountList(carrierAccount);
+    }
+
+    /**
+     * Add Carrier Account
+     * 
+     * @param carrierAccount Carrier Account
+     * @return result
+     */
+    @Override
+    public int insertCarrierAccount(CarrierAccount carrierAccount)
+    {
+        carrierAccount.setCreateTime(DateUtils.getNowDate());
+        return carrierAccountMapper.insertCarrierAccount(carrierAccount);
+    }
+
+    /**
+     * Update Carrier Account
+     * 
+     * @param carrierAccount Carrier Account
+     * @return result
+     */
+    @Override
+    public int updateCarrierAccount(CarrierAccount carrierAccount)
+    {
+        carrierAccount.setUpdateTime(DateUtils.getNowDate());
+        return carrierAccountMapper.updateCarrierAccount(carrierAccount);
+    }
+
+    /**
+     * Delete Carrier Account By ID
+     * 
+     * @param ids Entity ID
+     * @return result
+     */
+    @Override
+    public int deleteCarrierAccountByIds(String ids)
+    {
+        return carrierAccountMapper.deleteCarrierAccountByIds(Convert.toStrArray(ids));
+    }
+
+    /**
+     * Delete Carrier Account
+     * 
+     * @param id Carrier AccountID
+     * @return result
+     */
+    @Override
+    public int deleteCarrierAccountById(Long id)
+    {
+        return carrierAccountMapper.deleteCarrierAccountById(id);
+    }
+}
