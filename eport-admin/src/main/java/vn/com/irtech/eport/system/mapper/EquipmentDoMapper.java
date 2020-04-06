380cbd421e8ed3299759297ca0a62349b19a31b3
Tran Cong Minh <trancongminh1134@gmail.com>
2020-04-06 15:20:12 +0700

    + mapper, domain, service, controller from codegen

---
+++ eport-admin/src/main/java/vn/com/irtech/eport/system/mapper/EquipmentDoMapper.java
@@ -0,0 +1,61 @@
+package vn.com.irtech.eport.system.mapper;
+
+import java.util.List;
+import vn.com.irtech.eport.system.domain.EquipmentDo;
+
+/**
+ * Exchange Delivery OrderMapper Interface
+ * 
+ * @author ruoyi
+ * @date 2020-04-06
+ */
+public interface EquipmentDoMapper 
+{
+    /**
+     * Get Exchange Delivery Order
+     * 
+     * @param id Exchange Delivery OrderID
+     * @return Exchange Delivery Order
+     */
+    public EquipmentDo selectEquipmentDoById(Long id);
+
+    /**
+     * Get Exchange Delivery Order List
+     * 
+     * @param equipmentDo Exchange Delivery Order
+     * @return Exchange Delivery Order List
+     */
+    public List<EquipmentDo> selectEquipmentDoList(EquipmentDo equipmentDo);
+
+    /**
+     * Add Exchange Delivery Order
+     * 
+     * @param equipmentDo Exchange Delivery Order
+     * @return Result
+     */
+    public int insertEquipmentDo(EquipmentDo equipmentDo);
+
+    /**
+     * Update Exchange Delivery Order
+     * 
+     * @param equipmentDo Exchange Delivery Order
+     * @return Result
+     */
+    public int updateEquipmentDo(EquipmentDo equipmentDo);
+
+    /**
+     * Delete Exchange Delivery Order
+     * 
+     * @param id Exchange Delivery OrderID
+     * @return result
+     */
+    public int deleteEquipmentDoById(Long id);
+
+    /**
+     * Batch Delete Exchange Delivery Order
+     * 
+     * @param ids IDs
+     * @return result
+     */
+    public int deleteEquipmentDoByIds(String[] ids);
+}
---
+++ eport-carrier-service/src/main/java/vn/com/irtech/eport/carrier/mapper/CarrierEquipmentDoMapper.java
@@ -0,0 +1,61 @@
+package vn.com.irtech.eport.carrier.mapper;
+
+import java.util.List;
+import vn.com.irtech.eport.carrier.domain.CarrierEquipmentDo;
+
+
+/**
+ * Exchange Delivery OrderMapper Interface
+ * 
+ * @author ruoyi
+ * @date 2020-04-06
+ */
+public interface CarrierEquipmentDoMapper 
+{
+    /**
+     * Get Exchange Delivery Order
+     * 
+     * @param id Exchange Delivery OrderID
+     * @return Exchange Delivery Order
+     */
+    public CarrierEquipmentDo selectEquipmentDoById(Long id);
+
+    /**
+     * Get Exchange Delivery Order List
+     * 
+     * @param equipmentDo Exchange Delivery Order
+     * @return Exchange Delivery Order List
+     */
+    public List<CarrierEquipmentDo> selectEquipmentDoList(CarrierEquipmentDo equipmentDo);
+
+    /**
+     * Add Exchange Delivery Order
+     * 
+     * @param equipmentDo Exchange Delivery Order
+     * @return Result
+     */
+    public int insertEquipmentDo(CarrierEquipmentDo equipmentDo);
+
+    /**
+     * Update Exchange Delivery Order
+     * 
+     * @param equipmentDo Exchange Delivery Order
+     * @return Result
+     */
+    public int updateEquipmentDo(CarrierEquipmentDo equipmentDo);
+    /**
+     * Delete Exchange Delivery Order
+     * 
+     * @param id Exchange Delivery OrderID
+     * @return result
+     */
+    public int deleteEquipmentDoById(Long id);
+
+    /**
+     * Batch Delete Exchange Delivery Order
+     * 
+     * @param ids IDs
+     * @return result
+     */
+    public int deleteEquipmentDoByIds(String[] ids);
+}
