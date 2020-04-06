bbe9e05c5e75d70d1b796dedc98ba033e9c1d97b
GiapHD <giapmaster@gmail.com>
2020-04-06 10:21:30 +0700

    Add carrier group

---
+++ eport-carrier-service/src/main/java/vn/com/irtech/eport/carrier/mapper/CarrierGroupMapper.java
@@ -0,0 +1,61 @@
+package vn.com.irtech.eport.carrier.mapper;
+
+import java.util.List;
+import vn.com.irtech.eport.carrier.domain.CarrierGroup;
+
+/**
+ * Carrier GroupMapper Interface
+ * 
+ * @author irtech
+ * @date 2020-04-06
+ */
+public interface CarrierGroupMapper 
+{
+    /**
+     * Get Carrier Group
+     * 
+     * @param id Carrier GroupID
+     * @return Carrier Group
+     */
+    public CarrierGroup selectCarrierGroupById(Long id);
+
+    /**
+     * Get Carrier Group List
+     * 
+     * @param carrierGroup Carrier Group
+     * @return Carrier Group List
+     */
+    public List<CarrierGroup> selectCarrierGroupList(CarrierGroup carrierGroup);
+
+    /**
+     * Add Carrier Group
+     * 
+     * @param carrierGroup Carrier Group
+     * @return Result
+     */
+    public int insertCarrierGroup(CarrierGroup carrierGroup);
+
+    /**
+     * Update Carrier Group
+     * 
+     * @param carrierGroup Carrier Group
+     * @return Result
+     */
+    public int updateCarrierGroup(CarrierGroup carrierGroup);
+
+    /**
+     * Delete Carrier Group
+     * 
+     * @param id Carrier GroupID
+     * @return result
+     */
+    public int deleteCarrierGroupById(Long id);
+
+    /**
+     * Batch Delete Carrier Group
+     * 
+     * @param ids IDs
+     * @return result
+     */
+    public int deleteCarrierGroupByIds(String[] ids);
+}
