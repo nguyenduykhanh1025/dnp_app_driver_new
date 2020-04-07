da96df6d3b46177ec1b131002ab639566011849c
bluebird <nttruclol@gmail.com>
2020-04-07 10:58:28 +0700

    insert db carrier_group, view infor carrier

---
+++ sql/07_04_insert_carrier_group.sql
@@ -0,0 +1,6 @@
+INSERT INTO `carrier_group` (`id`, `group_code`, `group_name`, `operate_code`, `main_email`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES 
+('1', 'CMA', 'CMAA', 'CMA,CNC,APL,ANL,AEW', 'cma@cam-cgm.com; cnc_manager@gmai.com', '', NULL, '', NULL), 
+('2', 'EMC', 'EMC', 'EMC', 'emc@evergreen.com', '', NULL, '', NULL),
+('3', 'MSL', 'MSL', 'MSL,MCC,SUD,SCL', 'msl@maersk.com', '', NULL, '', NULL),
+('4', 'WHL', 'WHL', 'WHL', 'whl@wanhai.com', '', NULL, '', NULL),
+('5', 'VLC', 'VLC', 'VLC', 'vlc@gmail.com', '', NULL, '', NULL)
