421c27158b5f8c116b0a69749e2bcc8be7d0c843
Tran Cong Minh <trancongminh1134@gmail.com>
2020-04-10 14:45:32 +0700

    update check DO

--- eport-system/src/main/java/vn/com/irtech/eport/equipment/service/impl/EquipmentDoServiceImpl.java
+++ eport-system/src/main/java/vn/com/irtech/eport/equipment/service/impl/EquipmentDoServiceImpl.java
@@ -1,5 +1,6 @@
 package vn.com.irtech.eport.equipment.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
@@ -125,8 +126,17 @@ public class EquipmentDoServiceImpl implements IEquipmentDoService
       return equipmentDoMapper.getContainerNumberListByIds(Convert.toStrArray(ids));
     }
     
+    @Override
+    public  Date getDocumentReceiptDate(Long id)
+    {
+        return equipmentDoMapper.getDocumentReceiptDate(id);
+    }
 
-   
+    @Override
+    public String getStatus(Long id)
+    {
+        return equipmentDoMapper.getStatus(id);
+    }
 
    
 }
