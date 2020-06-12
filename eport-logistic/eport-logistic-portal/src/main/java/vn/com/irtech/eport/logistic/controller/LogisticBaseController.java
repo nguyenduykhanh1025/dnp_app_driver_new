3f417218756d257c09723d05dbb5369bc601e47f
Tran Cong Minh <trancongminh1134@gmail.com>
2020-06-12 21:02:35 +0700

    update sms brandname

--- eport-logistic/eport-logistic-portal/src/main/java/vn/com/irtech/eport/logistic/controller/LogisticBaseController.java
+++ eport-logistic/eport-logistic-portal/src/main/java/vn/com/irtech/eport/logistic/controller/LogisticBaseController.java
@@ -1,5 +1,12 @@
 package vn.com.irtech.eport.logistic.controller;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import vn.com.irtech.eport.common.core.controller.BaseController;
@@ -38,4 +45,31 @@ public abstract class LogisticBaseController extends BaseController {
 		}
 		return false;
 	}
+	public String postOtpMessage(String contentabc) throws IOException {
+		String url = "http://svc.netplus.vn/WSSendSMS.asmx";
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-Type", "text/xml;charset=UTF-8");
+		//String countryCode="Canada";
+		String xml = "<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\"><soap12:Body><SendSMS xmlns=\"http://tempuri.org/\"><aSMS_Input><SmsType>1</SmsType><IdCustomerSent>140273</IdCustomerSent><CompanyCode>DANANGPORT</CompanyCode><Mobile>84983960445</Mobile><SMSContent>"+contentabc+"</SMSContent></aSMS_Input><userName>danangportguitin</userName><password>568926</password></SendSMS></soap12:Body></soap12:Envelope>";
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(xml);
+		wr.flush();
+		wr.close();
+		String responseStatus = con.getResponseMessage();
+		System.out.println(responseStatus);
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+		con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+		while ((inputLine = in.readLine()) != null) {
+		response.append(inputLine);
+		}
+		in.close();
+			System.out.println("response:" + response.toString());
+			return response.toString();
+		}
+		
 }
