b0ec876d8f443d2e761acf2c2f4b6f792a4a0b9d
bluebird <nttruclol@gmail.com>
2020-06-16 19:12:19 +0700

    +add table assign, pickup_his, driver_truck

--- sql/16_06_add_table_truck.sql
+++ sql/16_06_add_table_truck.sql
@@ -12,3 +12,52 @@ CREATE TABLE IF NOT EXISTS `truck`(
     `update_time` datetime DEFAULT NULL COMMENT 'Update Time',
     PRIMARY KEY (`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Truck';
+
+CREATE TABLE IF NOT EXISTS `driver_truck`(
+    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `driver_id` biginr(20) NOT NULL COMMENT 'ID tài xế',
+    `truck_ids` varchar(100) NOT NULL COMMENT 'Chuỗi truck_id',
+    `remark` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT 'Ghi chu',
+    `create_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT 'Create By',
+    `create_time` datetime DEFAULT NULL COMMENT 'Create Time',
+    `update_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT 'Update By',
+    `update_time` datetime DEFAULT NULL COMMENT 'Update Time',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='driver_truck';
+
+CREATE TABLE IF NOT EXISTS `assign`(
+    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `logistic_group_id` bigint(20) NULL COMMENT 'Logistic Group',
+    `shipment_id` bigint(20) NOT NULL COMMENT 'Ma Lo',
+    `container_no_list` text COLLATE utf8_bin NULL COMMENT 'Chuỗi Container Number',
+    `driver_id` bigint(20) NOT NULL COMMENT 'ID tài xế',
+    `phone_number` varchar(15)  NULL COMMENT 'số điện thoại',
+    `full_name` varchar(50)  NULL COMMENT 'Họ và Tên',
+    'tractor_truck' varchar(15) NULL COMMENT 'Biển số xe đầu kéo',
+    'trailer_truck' varchar(15) NULL COMMENT 'Biển số xe rơ mooc',
+    `remark` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT 'Ghi chu',
+    `create_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT 'Create By',
+    `create_time` datetime DEFAULT NULL COMMENT 'Create Time',
+    `update_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT 'Update By',
+    `update_time` datetime DEFAULT NULL COMMENT 'Update Time',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='driver_truck';
+
+CREATE TABLE IF NOT EXISTS `pickup_his`(
+    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `logistic_group_id` bigint(20) NULL COMMENT 'Logistic Group',
+    `shipment_id` bigint(20) NOT NULL COMMENT 'Ma Lo',
+    `container_no_list` text COLLATE utf8_bin NULL COMMENT 'Chuỗi Container Number',
+    `driver_id` bigint(20) NOT NULL COMMENT 'ID tài xế',
+    `phone_number` varchar(15)  NULL COMMENT 'số điện thoại',
+    `full_name` varchar(50)  NULL COMMENT 'Họ và Tên',
+    'tractor_truck' varchar(15) NULL COMMENT 'Biển số xe đầu kéo',
+    'trailer_truck' varchar(15) NULL COMMENT 'Biển số xe rơ mooc',
+    'coordinate' varchar(20) NULL COMMENT 'Tọa độ cont',
+    `remark` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT 'Ghi chu',
+    `create_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT 'Create By',
+    `create_time` datetime DEFAULT NULL COMMENT 'Create Time',
+    `update_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT 'Update By',
+    `update_time` datetime DEFAULT NULL COMMENT 'Update Time',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='driver_truck';
