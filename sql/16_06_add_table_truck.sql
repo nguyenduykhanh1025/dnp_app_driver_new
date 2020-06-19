71a7829810e05d721b67ed28d452e4e2e3c0b823
Truc Nguyen <nttruclol@gmail.com>
2020-06-19 01:43:01 +0000

    Thanhtruc

--- sql/16_06_add_table_truck.sql
+++ sql/16_06_add_table_truck.sql
@@ -14,22 +14,16 @@ CREATE TABLE IF NOT EXISTS `truck`(
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Truck';
 
 CREATE TABLE IF NOT EXISTS `driver_truck`(
-    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
     `driver_id` bigint(20) NOT NULL COMMENT 'ID tài xế',
-    `truck_id` varchar(100) NOT NULL COMMENT 'truck_id',
-    `remark` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT 'Ghi chu',
-    `create_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT 'Create By',
-    `create_time` datetime DEFAULT NULL COMMENT 'Create Time',
-    `update_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT 'Update By',
-    `update_time` datetime DEFAULT NULL COMMENT 'Update Time',
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='driver_truck';
+    `truck_id` bigint(20) NOT NULL COMMENT 'truck_id',
+    PRIMARY KEY (`driver_id`,`truck_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='driver_truck';
 
 CREATE TABLE IF NOT EXISTS `assign`(
     `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
     `logistic_group_id` bigint(20) NULL COMMENT 'Logistic Group',
     `shipment_id` bigint(20) NOT NULL COMMENT 'Ma Lo',
-    `container_no_list` text COLLATE utf8_bin NULL COMMENT 'Chuỗi Container Number',
+    `shipment_detail_id` bigint(20) COLLATE utf8_bin NULL COMMENT 'Container id',
     `driver_id` bigint(20) NOT NULL COMMENT 'ID tài xế',
     `phone_number` varchar(15)  NULL COMMENT 'số điện thoại',
     `full_name` varchar(50)  NULL COMMENT 'Họ và Tên',
@@ -47,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `pickup_his`(
     `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
     `logistic_group_id` bigint(20) NULL COMMENT 'Logistic Group',
     `shipment_id` bigint(20) NOT NULL COMMENT 'Ma Lo',
-    `container_no_list` text COLLATE utf8_bin NULL COMMENT 'Chuỗi Container Number',
+    `shipment_detail_id` bigint(20) COLLATE utf8_bin NULL COMMENT 'Container id',
     `driver_id` bigint(20) NOT NULL COMMENT 'ID tài xế',
     `phone_number` varchar(15)  NULL COMMENT 'số điện thoại',
     `full_name` varchar(50)  NULL COMMENT 'Họ và Tên',
