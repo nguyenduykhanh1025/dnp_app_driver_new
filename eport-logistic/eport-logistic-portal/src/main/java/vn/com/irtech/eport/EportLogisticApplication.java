178f2aed0680d0474af0adf64e35037a2eb0bf6d
Trong Hieu <tronghieu8531@gmail.com>
2020-06-26 22:29:22 +0700

    - Update

--- eport-logistic/eport-logistic-portal/src/main/java/vn/com/irtech/eport/EportLogisticApplication.java
+++ eport-logistic/eport-logistic-portal/src/main/java/vn/com/irtech/eport/EportLogisticApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import vn.com.irtech.eport.framework.custom.queue.listener.CheckingCustomThread;
+
 /**
  * @author admin
  */
