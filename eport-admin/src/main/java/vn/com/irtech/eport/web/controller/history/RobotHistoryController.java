f6b8fdf3fbfbdc804fa5ffdfe5227bb3b6e3dc7c
Anh Tài <nqat2003@gmail.com>
2020-07-02 09:16:57 +0700

    - Update UI screen robot history and notification

---
+++ eport-admin/src/main/java/vn/com/irtech/eport/web/controller/history/RobotHistoryController.java
@@ -0,0 +1,36 @@
+package vn.com.irtech.eport.web.controller.history;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import vn.com.irtech.eport.common.core.controller.BaseController;
+import vn.com.irtech.eport.common.core.page.TableDataInfo;
+import vn.com.irtech.eport.logistic.domain.ProcessHistory;
+import vn.com.irtech.eport.logistic.service.IProcessHistoryService;
+
+@Controller
+@RequestMapping("/history/robot")
+public class RobotHistoryController extends BaseController {
+    private final static String PREFIX = "history/robot";
+
+    @Autowired
+    private IProcessHistoryService processHistoryService;
+    
+    @GetMapping("/")
+	public String getTruckHistory() {
+		return PREFIX + "/index";
+    }
+    
+    @GetMapping("/list")
+	@ResponseBody
+	public TableDataInfo getListTruckRobot(ProcessHistory processHistory) {
+		startPage();
+		List<ProcessHistory> pickupHistorys = processHistoryService.selectProcessHistoryList(processHistory);
+		return getDataTable(pickupHistorys);
+	}
+}
