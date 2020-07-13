573b7b975a396c47e5541bcf62e1dd20a2373e1d
Anh Tai <nqat2003@gmail.com>
2020-07-13 21:05:28 +0700

    - Update view payment in OM support

--- eport-admin/src/main/resources/static/eport/js/modules/om/orderRegistrationSupport/paymentSupport.js
+++ eport-admin/src/main/resources/static/eport/js/modules/om/orderRegistrationSupport/paymentSupport.js
@@ -1,18 +1,35 @@
-"use strict";
-const PREFIX = ctx + "om/order/support";
+'use strict';
+const PREFIX = ctx + 'om/order/support';
+const DOCUMENT_HEIGHT = $(document).height();
+var tableQnt = 3;
 
 $(document).ready(function () {
-  $("#dgOrder").datagrid({
-    height: $(document).height()/2 - 70,
-    singleSelect: true,
-    clientPaging: false,
-    pagination: false,
-    rownumbers: true,
-    nowrap: false,
-    striped: true,
-    loader: function (param, success, error) {
-      success(orderList);
-    },
-  });
+  for (let i = 1; i < tableQnt; i++) {
+    let divClone = $('div#dgOrder' + i);
+    let totalLabel = divClone.find('.txt-total');
+    $(totalLabel).text(formatCurency(123456789) + ' VNĐ ');
+    $('#dgOrder' + i + ' table').datagrid({
+      height: DOCUMENT_HEIGHT / 2 - 70,
+      width: $(document).width() - 50,
+      singleSelect: true,
+      clientPaging: false,
+      pagination: false,
+      rownumbers: true,
+      nowrap: false,
+      striped: true,
+      loader: function (param, success, error) {
+        success(orderList);
+      },
+    });
+    let currentId = i + 1;
+    let clonned = divClone.clone().prop('id', 'dgOrder' + currentId);
+    divClone.after(clonned);
+  }
+ 
+  $('.content').height(DOCUMENT_HEIGHT - $('.header').height() - 20);
 });
+
+function formatCurency(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+}
 
