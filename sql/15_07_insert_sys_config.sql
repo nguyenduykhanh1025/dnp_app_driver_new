dd8a44143abb9208df0c376247263d7d4b93fa3b
bluebird <nttruclol@gmail.com>
2020-07-15 09:01:21 +0700

    + add acciss api sql sys_config

---
+++ sql/15_07_insert_sys_config.sql
@@ -0,0 +1,2 @@
+INSERT INTO `sys_config` (`config_id`, `config_name`, `config_key`, `config_value`, `config_type`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES
+(103, 'Acciss api uri', 'acciss.api.uri', 'http://192.168.0.36:8060/ACCIS-Web/rest/v1/eportcontroller/getCustomsStatus/', 'Y', 'admin', '2020-07-15 08:57:59', '', NULL, NULL);
