6663637f87d0005d1146a1b058be83972d98b1c4
Trong Hieu <tronghieu8531@gmail.com>
2020-07-29 20:36:12 +0700

    - Change ip redis to local server

--- eport-api/src/main/java/vn/com/irtech/eport/api/EportApiApplication.java
+++ eport-api/src/main/java/vn/com/irtech/eport/api/EportApiApplication.java
@@ -2,7 +2,9 @@ package vn.com.irtech.eport.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "vn.com.irtech.eport")
 @SpringBootApplication
 public class EportApiApplication {
 	public static void main(String[] args) {
