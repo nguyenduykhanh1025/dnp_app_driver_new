c9df15e7727e402f88c686c7df08b4d6fadb0652
Trong Hieu <tronghieu8531@gmail.com>
2020-07-31 08:06:29 +0700

    ! Fix duplicate vessel

--- eport-logistic/eport-logistic-portal/src/main/resources/static/js/logistic/sendContEmpty/index.js
+++ eport-logistic/eport-logistic-portal/src/main/resources/static/js/logistic/sendContEmpty/index.js
@@ -652,6 +652,9 @@ function reloadShipmentDetail() {
         $('#check'+i).prop('checked', false);
     }
     $("#deleteBtn").prop("disabled", true);
+    $("#verifyBtn").prop("disabled", true);
+    $("#payBtn").prop("disabled", true);
+    $("#exportBillBtn").prop("disabled", true);
     setLayoutRegisterStatus();
     loadShipmentDetail(shipmentSelected.id);
 }
