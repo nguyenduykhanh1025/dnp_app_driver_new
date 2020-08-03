ce7cd03a5bc378fd9b79b3271d649723e11f4025
Tran Cong Minh <trancongminh1134@gmail.com>
2020-08-03 14:53:27 +0700

    fix bug update, flag eDO

--- eport-carrier-portal/src/main/resources/static/eport/js/modules/edo/edo.js
+++ eport-carrier-portal/src/main/resources/static/eport/js/modules/edo/edo.js
@@ -119,11 +119,13 @@ function formatToYDMHMS(date) {
 }
 
 function formatAction(value, row, index) {
-  var actions = [];
-  if(row.status == 1)
+  let actions = [];
+  let disabled = "";
+  if(row.id == 3)
   {
-    actions.push('<a class="btn btn-success btn-xs btn-action mt5" onclick="viewUpdateCont(\'' + row.id + '\')"><i class="fa fa-pencil-square-o"></i> Cập nhật</a> ' + '<br>');
+    disabled = "disabled";
   }
+  actions.push('<button ' + disabled + ' class="btn btn-success btn-xs btn-action mt5 mb5" id="updateEdo" onclick="viewUpdateCont(\'' + row.id + '\')"><i class="fa fa-pencil-square-o"></i> Cập Nhật</button>' + '<br>');
   actions.push('<a class="btn btn-info btn-xs btn-xs btn-action mt5 mb5" onclick="viewHistoryCont(\'' + row.id + '\')"><i class="fa fa-history"></i> Lịch sử</a> ');
   return actions.join("");
 }
@@ -250,12 +252,12 @@ function searchInfoEdo() {
 
 function formatStatus(value) {
   switch (value) {
-    case 0:
+    case '1':
       return "<span class='label label-success'>Chưa làm lệnh</span>";
-    case 1:
+    case '2':
       return "<span class='label label-success'>Đã làm lệnh</span>";
-    default:
-      return "<span class='label label-warning'>Gate-in</span>";
+    case '3':
+    return "<span class='label label-success'>Gate-in</span>";
   }
 }
 
