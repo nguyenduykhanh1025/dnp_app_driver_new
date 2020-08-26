25d25dc4fc436cfdc3af9118144694e107c98fc2
Trong Hieu <tronghieu8531@gmail.com>
2020-08-26 09:21:41 +0700

    - Update api logistic

--- eport-api/src/main/java/vn/com/irtech/eport/api/controller/logistic/LogisticCommonController.java
+++ eport-api/src/main/java/vn/com/irtech/eport/api/controller/logistic/LogisticCommonController.java
@@ -1,9 +1,15 @@
 package vn.com.irtech.eport.api.controller.logistic;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
@@ -12,7 +18,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import vn.com.irtech.eport.api.form.ShipmentForm;
 import vn.com.irtech.eport.api.util.SecurityUtils;
@@ -20,12 +29,17 @@ import vn.com.irtech.eport.common.core.domain.AjaxResult;
 import vn.com.irtech.eport.common.utils.DateUtils;
 import vn.com.irtech.eport.common.utils.bean.BeanUtils;
 import vn.com.irtech.eport.logistic.domain.LogisticAccount;
+import vn.com.irtech.eport.logistic.domain.LogisticGroup;
+import vn.com.irtech.eport.logistic.domain.OtpCode;
 import vn.com.irtech.eport.logistic.domain.ProcessBill;
 import vn.com.irtech.eport.logistic.domain.Shipment;
+import vn.com.irtech.eport.logistic.domain.ShipmentDetail;
 import vn.com.irtech.eport.logistic.service.ICatosApiService;
 import vn.com.irtech.eport.logistic.service.ILogisticAccountService;
 import vn.com.irtech.eport.logistic.service.INapasApiService;
+import vn.com.irtech.eport.logistic.service.IOtpCodeService;
 import vn.com.irtech.eport.logistic.service.IProcessBillService;
+import vn.com.irtech.eport.logistic.service.IShipmentDetailService;
 import vn.com.irtech.eport.logistic.service.IShipmentService;
 import vn.com.irtech.eport.system.service.ISysConfigService;
 
@@ -52,6 +66,12 @@ public class LogisticCommonController extends LogisticBaseController {
 	
 	@Autowired
 	private ICatosApiService catosApiService;
+	
+	@Autowired 
+	private IOtpCodeService otpCodeService;
+	
+	@Autowired
+	private IShipmentDetailService shipmentDetailService;
 	
 	class ProcessIdComparator implements Comparator<ProcessBill> {
         public int compare(ProcessBill processBill1, ProcessBill processBill2) {
@@ -107,6 +127,56 @@ public class LogisticCommonController extends LogisticBaseController {
 		return ajaxResult;
 	}
 	
+	@GetMapping("/shipment/{shipmentId}/otp")
+	@ResponseBody
+	public AjaxResult sendOTP(@PathVariable("shipmentId") Long shipmentId) {
+		LogisticGroup lGroup = getLogisticGroup();
+
+		OtpCode otpCode = new OtpCode();
+		Random rd = new Random();
+		long rD = rd.nextInt(900000)+100000;
+		String tDCode = Long.toString(rD);
+		
+		ShipmentDetail shipmentDetailParam = new ShipmentDetail();
+		shipmentDetailParam.setProcessStatus("N");
+		shipmentDetailParam.setUserVerifyStatus("N");
+		shipmentDetailParam.setCustomStatus("R");
+		shipmentDetailParam.setShipmentId(shipmentId);
+		List<ShipmentDetail> shipmentDetails = shipmentDetailService.selectShipmentDetailList(shipmentDetailParam);
+		String shipmentDetailIds = "";
+		for (ShipmentDetail shipmentDetail : shipmentDetails) {
+			shipmentDetailIds += shipmentDetail.getId() + ",";
+		}
+		shipmentDetailIds.substring(0, shipmentDetailIds.length()-1);
+		
+		otpCodeService.deleteOtpCodeByShipmentDetailIds(shipmentDetailIds);
+
+		otpCode.setTransactionId(shipmentDetailIds);
+		otpCode.setPhoneNumber(lGroup.getMobilePhone());
+		otpCode.setOtpCode(tDCode);
+		otpCode.setOtpType("1");
+
+		Calendar cal = Calendar.getInstance();
+		Date now = new Date();
+		cal.setTime(now);
+		cal.add(Calendar.MINUTE, +5);
+		otpCode.setExpiredTime(cal.getTime());
+		otpCodeService.insertSysOtp(otpCode);
+		// FIXME Get message template from SysConfigService, using String.format to replace place holders
+		String content = configService.selectConfigByKey("otp.format");
+		content = content.replace("{shipmentId}", shipmentId.toString()).replace("{otp}", tDCode);
+		String response = "";
+		 try {
+		 	response = otpCodeService.postOtpMessage(lGroup.getMobilePhone(), content);
+		 	logger.debug("OTP Send Response: " + response);
+		 } catch (IOException ex) {
+		 	// process the exception
+			 logger.error(ex.getMessage());
+		 }
+		return AjaxResult.success();
+	}
+
+	
 	@GetMapping("/shipment/{shipmentId}/payment")
 	public AjaxResult paymentForm(@PathVariable Long shipmentId) {
 		LogisticAccount logisticAccount = logisticAccountService.selectLogisticAccountById(SecurityUtils.getCurrentUser().getUser().getUserId());
@@ -117,7 +187,6 @@ public class LogisticCommonController extends LogisticBaseController {
 		if (CollectionUtils.isEmpty(shipments)) {
 			// Add messeage text
 			return error();
-			// return "error/unauth";
 		}
 		
 		ProcessBill processBillParam = new ProcessBill();
@@ -128,7 +197,6 @@ public class LogisticCommonController extends LogisticBaseController {
 		if (CollectionUtils.isEmpty(processBills)) {
 			// Shipment Doesn't have any bill need to pay
 			return error();
-			// return "error/404";
 		}
 		
 		// Sort processBills by processOrderId
@@ -152,11 +220,10 @@ public class LogisticCommonController extends LogisticBaseController {
 		orderId += "-" + DateUtils.dateTimeNow();
 		
 		AjaxResult ajaxResult = AjaxResult.success();
-		
-		ajaxResult.put("resultUrl", configService.selectConfigByKey("napas.payment.result"));
+		ajaxResult.put("resultUrl", configService.selectConfigByKey("napas.payment.mobile.result"));
 		ajaxResult.put("referenceOrder", "Thanh toan " + orderId);
-		// TODO : get ip user
-		String clientIp = "x.x.x.x";
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		String clientIp = request.getRemoteAddr();
 		ajaxResult.put("clientIp", clientIp);
 		ajaxResult.put("data", napasApiService.getDataKey(clientIp, "deviceId", orderId, total, napasApiService.getAccessToken()));
 		
