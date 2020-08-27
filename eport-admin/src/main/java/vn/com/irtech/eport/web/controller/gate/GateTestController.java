2cab20eb6d7d5167ba90af37fc257c8328622464
Trong Hieu <tronghieu8531@gmail.com>
2020-08-27 20:35:32 +0700

    - Update

--- eport-admin/src/main/java/vn/com/irtech/eport/web/controller/gate/GateTestController.java
+++ eport-admin/src/main/java/vn/com/irtech/eport/web/controller/gate/GateTestController.java
@@ -1,9 +1,16 @@
 package vn.com.irtech.eport.web.controller.gate;
 
+import java.util.Date;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,20 +18,60 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 
+import vn.com.irtech.eport.common.constant.EportConstants;
 import vn.com.irtech.eport.common.core.controller.BaseController;
 import vn.com.irtech.eport.common.core.domain.AjaxResult;
 import vn.com.irtech.eport.common.utils.CacheUtils;
+import vn.com.irtech.eport.common.utils.StringUtils;
+import vn.com.irtech.eport.common.utils.bean.BeanUtils;
 import vn.com.irtech.eport.framework.web.service.WebSocketService;
+import vn.com.irtech.eport.logistic.domain.DriverAccount;
+import vn.com.irtech.eport.logistic.domain.LogisticGroup;
+import vn.com.irtech.eport.logistic.domain.PickupAssign;
+import vn.com.irtech.eport.logistic.domain.PickupHistory;
+import vn.com.irtech.eport.logistic.domain.Shipment;
+import vn.com.irtech.eport.logistic.domain.ShipmentDetail;
+import vn.com.irtech.eport.logistic.service.ICatosApiService;
+import vn.com.irtech.eport.logistic.service.IDriverAccountService;
+import vn.com.irtech.eport.logistic.service.ILogisticGroupService;
+import vn.com.irtech.eport.logistic.service.IPickupAssignService;
+import vn.com.irtech.eport.logistic.service.IPickupHistoryService;
+import vn.com.irtech.eport.logistic.service.IShipmentDetailService;
+import vn.com.irtech.eport.logistic.service.IShipmentService;
 import vn.com.irtech.eport.web.dto.DetectionInfomation;
+import vn.com.irtech.eport.web.dto.GateInTestDataReq;
 
 @Controller
 @RequestMapping("/gate/test")
 public class GateTestController extends BaseController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(GateTestController.class);
 
 	private final static String PREFIX = "gate/test";
 	
 	@Autowired
 	private WebSocketService webSocketService;
+	
+	@Autowired
+	private ILogisticGroupService logisticGroupService;
+	
+	@Autowired
+	private IDriverAccountService driverAccountService;
+	
+	@Autowired
+	private IShipmentService shipmentService;
+	
+	@Autowired
+	private IShipmentDetailService shipmentDetailService;
+	
+	@Autowired
+	private IPickupHistoryService pickupHistoryService;
+	
+	@Autowired
+	private IPickupAssignService pickupAssignService;
+	
+	@Autowired
+	private ICatosApiService catosApiService;
 	
 	@GetMapping()
 	public String getView() {
@@ -42,6 +89,253 @@ public class GateTestController extends BaseController {
 		
 		// Send to monitor
 		webSocketService.sendMessage("/gate/detection/monitor", detectionInfo);
+		return success();
+	}
+	
+	@PostMapping("/logistics")
+	@ResponseBody
+	public List<LogisticGroup> getLogisticGroups() {
+		LogisticGroup logisticGroup = new LogisticGroup();
+	    logisticGroup.setGroupName("Chọn đơn vị Logistics");
+	    logisticGroup.setId(0L);
+	    LogisticGroup logisticGroupParam = new LogisticGroup();
+	    logisticGroupParam.setDelFlag("0");
+	    List<LogisticGroup> logisticGroups = logisticGroupService.selectLogisticGroupList(logisticGroupParam);
+	    logisticGroups.add(0, logisticGroup);
+	    return logisticGroups;
+	}
+	
+	@PostMapping("/logistic/{groupId}/drivers")
+	@ResponseBody
+	public List<DriverAccount> getDriverAccounts(@PathVariable("groupId") Long groupId) {
+		DriverAccount driverAccount = new DriverAccount();
+		driverAccount.setLogisticGroupId(groupId);
+		return driverAccountService.selectDriverAccountList(driverAccount);
+	}
+	
+	@PostMapping("/gateIn")
+	@Transactional
+	@ResponseBody
+	public AjaxResult gateIn(@Validated @RequestBody GateInTestDataReq gateInTestDataReq) {
+		
+		// Sample data
+		Shipment shipment = new Shipment();
+		shipment.setLogisticAccountId(1L);
+		shipment.setLogisticGroupId(gateInTestDataReq.getLogisticGroupId());
+		shipment.setTaxCode("3123123123");
+		shipment.setContainerAmount(2L);
+		
+		ShipmentDetail shipmentDetail = new ShipmentDetail();
+		shipmentDetail.setLogisticGroupId(gateInTestDataReq.getLogisticGroupId());
+		shipmentDetail.setFe("F");
+		shipmentDetail.setVslNm("test");
+		shipmentDetail.setVoyNo("test");
+		shipmentDetail.setOpeCode("test");
+		shipmentDetail.setPaymentStatus("Y");
+		shipmentDetail.setProcessStatus("Y");
+		shipmentDetail.setUserVerifyStatus("Y");
+		shipmentDetail.setStatus(4);
+		shipmentDetail.setCustomStatus("Y");
+		shipmentDetail.setDoReceivedTime(new Date());
+		shipmentDetail.setDoStatus("Y");
+		shipmentDetail.setSztp("22G0");
+		
+		if (gateInTestDataReq.getSendOption()) {
+			if (StringUtils.isNotEmpty(gateInTestDataReq.getContainerSend1())) {
+				logger.debug("Drop container 1: " + gateInTestDataReq.getContainerSend1());
+				Shipment shipment1 = new Shipment();
+				BeanUtils.copyBeanProp(shipment1, shipment);
+				shipment1.setBookingNo("test");
+				shipment1.setServiceType(EportConstants.SERVICE_DROP_FULL);
+				shipmentService.insertShipment(shipment1);
+				
+				ShipmentDetail shipmentDetail1 = new ShipmentDetail();
+				BeanUtils.copyBeanProp(shipmentDetail1, shipmentDetail);
+				shipmentDetail1.setContainerNo(gateInTestDataReq.getContainerSend1());
+				shipmentDetail1.setBookingNo("test");
+				shipmentDetail1.setShipmentId(shipment1.getId());
+				shipmentDetailService.insertShipmentDetail(shipmentDetail1);
+				
+				PickupAssign pickupAssign1 = new PickupAssign();
+				pickupAssign1.setLogisticGroupId(gateInTestDataReq.getLogisticGroupId());
+				pickupAssign1.setShipmentId(shipment1.getId());
+				pickupAssign1.setDriverId(gateInTestDataReq.getDriverId());
+				pickupAssign1.setShipmentDetailId(shipmentDetail1.getId());
+				pickupAssign1.setTruckNo(gateInTestDataReq.getTruckNo());
+				pickupAssign1.setChassisNo(gateInTestDataReq.getChassisNo());
+				
+				pickupAssignService.insertPickupAssign(pickupAssign1);
+				
+				PickupHistory pickupHistory1 = new PickupHistory();
+				pickupHistory1.setLogisticGroupId(gateInTestDataReq.getLogisticGroupId());
+				pickupHistory1.setShipmentId(shipment1.getId());
+				pickupHistory1.setDriverId(gateInTestDataReq.getDriverId());
+				pickupHistory1.setPickupAssignId(pickupAssign1.getId());
+				pickupHistory1.setTruckNo(gateInTestDataReq.getTruckNo());
+				pickupHistory1.setChassisNo(gateInTestDataReq.getChassisNo());
+				pickupHistory1.setContainerNo(gateInTestDataReq.getContainerSend1());
+				pickupHistory1.setStatus(0);
+				pickupHistory1.setShipmentId(shipmentDetail1.getId());
+				pickupHistory1.setGatePass(gateInTestDataReq.getGatePass());
+				pickupHistoryService.insertPickupHistory(pickupHistory1);
+				
+				logger.debug("Complete prepare data for drop container 1");
+				if (StringUtils.isNotEmpty(gateInTestDataReq.getContainerSend2())) {
+					logger.debug("Drop full with container 2: " + gateInTestDataReq.getContainerSend2());
+					ShipmentDetail shipmentDetail2 = new ShipmentDetail();
+					BeanUtils.copyBeanProp(shipmentDetail2, shipmentDetail);
+					shipmentDetail2.setContainerNo(gateInTestDataReq.getContainerSend2());
+					shipmentDetail2.setShipmentId(shipment1.getId());
+					shipmentDetail2.setBookingNo("test");
+					shipmentDetailService.insertShipmentDetail(shipmentDetail2);
+					
+					PickupAssign pickupAssign2 = new PickupAssign();
+					BeanUtils.copyBeanProp(pickupAssign2, pickupAssign1);
+					pickupAssign2.setShipmentDetailId(shipmentDetail2.getId());
+					pickupAssign2.setId(null);
+					pickupAssignService.insertPickupAssign(pickupAssign2);
+					
+					PickupHistory pickupHistory2 = new PickupHistory();
+					BeanUtils.copyBeanProp(pickupHistory2, pickupHistory1);
+					pickupHistory2.setShipmentDetailId(shipmentDetail2.getShipmentId());
+					pickupHistory2.setId(null);
+					pickupHistory2.setContainerNo(gateInTestDataReq.getContainerSend2());
+					pickupHistory2.setPickupAssignId(pickupAssign2.getId());
+					pickupHistoryService.insertPickupHistory(pickupHistory2);
+					
+					logger.debug("Complete prepare data for drop container 2");
+				}
+			}
+		}
+		
+		if (gateInTestDataReq.getReceiveOption()) {
+			
+			Shipment shipment2 = new Shipment();
+			BeanUtils.copyBeanProp(shipment2, shipment);
+			shipment2.setBlNo("test");
+			shipment2.setServiceType(EportConstants.SERVICE_PICKUP_FULL);
+			shipment2.setEdoFlg("0");
+			shipmentService.insertShipment(shipment2);
+			Long contId1 = 0L;
+			Long contId2 = 0L;
+			List<ShipmentDetail> shipmentDetails = catosApiService.selectShipmentDetailsByBLNo(gateInTestDataReq.getBlNo());
+			for (ShipmentDetail shipmentDetail1 : shipmentDetails) {
+				ShipmentDetail shipmentDetail2 = new ShipmentDetail();
+				BeanUtils.copyBeanProp(shipmentDetail2, shipmentDetail1);
+				shipmentDetail2.setLogisticGroupId(gateInTestDataReq.getLogisticGroupId());
+				shipmentDetail2.setFe("F");
+				if (StringUtils.isEmpty(shipmentDetail2.getVslNm())) {
+					shipmentDetail2.setVslNm("test");
+				}
+				
+				if (StringUtils.isEmpty(shipmentDetail2.getVoyNo())) {
+					shipmentDetail2.setVoyNo("test");
+				}
+				
+				if (StringUtils.isEmpty(shipmentDetail2.getOpeCode())) {
+					shipmentDetail2.setOpeCode("test");
+				}
+				
+				if (StringUtils.isEmpty(shipmentDetail2.getSztp())) {
+					shipmentDetail2.setSztp("22G0");
+				}
+				shipmentDetail2.setPaymentStatus("Y");
+				shipmentDetail2.setProcessStatus("Y");
+				shipmentDetail2.setUserVerifyStatus("Y");
+				shipmentDetail2.setStatus(4);
+				shipmentDetail2.setCustomStatus("Y");
+				shipmentDetail2.setDoReceivedTime(new Date());
+				shipmentDetail2.setPreorderPickup("Y");
+				shipmentDetail2.setPrePickupPaymentStatus("Y");
+				shipmentDetail2.setDoStatus("Y");
+				shipmentDetail2.setBlNo(gateInTestDataReq.getBlNo());
+				shipmentDetail2.setShipmentId(shipment2.getId());
+				shipmentDetailService.insertShipmentDetail(shipmentDetail2);
+				if (StringUtils.isNotEmpty(gateInTestDataReq.getContainerReceive1())) {
+					if (shipmentDetail2.getContainerNo().equals(gateInTestDataReq.getContainerReceive1())) {
+						contId1 = shipmentDetail2.getId();
+					}
+				}
+				if (StringUtils.isNotEmpty(gateInTestDataReq.getContainerReceive2())) {
+					if (shipmentDetail2.getContainerNo().equals(gateInTestDataReq.getContainerReceive2())) {
+						contId2 = shipmentDetail2.getId();
+					}
+				}
+			}
+			
+			if ("1".equals(gateInTestDataReq.getContainerFlg())) {
+				if (StringUtils.isNotEmpty(gateInTestDataReq.getContainerReceive1())) {
+					logger.debug("Pick container 1: " + gateInTestDataReq.getContainerReceive1());
+					
+					PickupAssign pickupAssign1 = new PickupAssign();
+					pickupAssign1.setLogisticGroupId(gateInTestDataReq.getLogisticGroupId());
+					pickupAssign1.setShipmentId(shipment2.getId());
+					pickupAssign1.setDriverId(gateInTestDataReq.getDriverId());
+					pickupAssign1.setShipmentDetailId(contId1);
+					pickupAssign1.setTruckNo(gateInTestDataReq.getTruckNo());
+					pickupAssign1.setChassisNo(gateInTestDataReq.getChassisNo());
+					
+					pickupAssignService.insertPickupAssign(pickupAssign1);
+					
+					PickupHistory pickupHistory1 = new PickupHistory();
+					pickupHistory1.setLogisticGroupId(gateInTestDataReq.getLogisticGroupId());
+					pickupHistory1.setShipmentId(shipment2.getId());
+					pickupHistory1.setDriverId(gateInTestDataReq.getDriverId());
+					pickupHistory1.setPickupAssignId(pickupAssign1.getId());
+					pickupHistory1.setTruckNo(gateInTestDataReq.getTruckNo());
+					pickupHistory1.setChassisNo(gateInTestDataReq.getChassisNo());
+					pickupHistory1.setContainerNo(gateInTestDataReq.getContainerReceive1());
+					pickupHistory1.setStatus(0);
+					pickupHistory1.setShipmentId(contId1);
+					pickupHistory1.setGatePass(gateInTestDataReq.getGatePass());
+					pickupHistoryService.insertPickupHistory(pickupHistory1);
+					
+					logger.debug("Complete prepare data for pick container 1");
+					if (StringUtils.isNotEmpty(gateInTestDataReq.getContainerReceive2())) {
+						logger.debug("pick full with container 2: " + gateInTestDataReq.getContainerReceive2());
+						PickupAssign pickupAssign2 = new PickupAssign();
+						BeanUtils.copyBeanProp(pickupAssign2, pickupAssign1);
+						pickupAssign2.setShipmentDetailId(contId2);
+						pickupAssign2.setId(null);
+						pickupAssignService.insertPickupAssign(pickupAssign2);
+						
+						PickupHistory pickupHistory2 = new PickupHistory();
+						BeanUtils.copyBeanProp(pickupHistory2, pickupHistory1);
+						pickupHistory2.setShipmentDetailId(contId2);
+						pickupHistory2.setId(null);
+						pickupHistory2.setContainerNo(gateInTestDataReq.getContainerReceive2());
+						pickupHistory2.setPickupAssignId(pickupAssign2.getId());
+						pickupHistoryService.insertPickupHistory(pickupHistory2);
+						
+						logger.debug("Complete prepare data for drop container 2");
+					}
+				}
+			} else {
+				for (int i=0; i<gateInTestDataReq.getContainerAmount(); i++) {
+					PickupAssign pickupAssign1 = new PickupAssign();
+					pickupAssign1.setLogisticGroupId(gateInTestDataReq.getLogisticGroupId());
+					pickupAssign1.setShipmentId(shipment2.getId());
+					pickupAssign1.setDriverId(gateInTestDataReq.getDriverId());
+					pickupAssign1.setTruckNo(gateInTestDataReq.getTruckNo());
+					pickupAssign1.setChassisNo(gateInTestDataReq.getChassisNo());
+					
+					pickupAssignService.insertPickupAssign(pickupAssign1);
+					
+					PickupHistory pickupHistory1 = new PickupHistory();
+					pickupHistory1.setLogisticGroupId(gateInTestDataReq.getLogisticGroupId());
+					pickupHistory1.setShipmentId(shipment2.getId());
+					pickupHistory1.setDriverId(gateInTestDataReq.getDriverId());
+					pickupHistory1.setPickupAssignId(pickupAssign1.getId());
+					pickupHistory1.setTruckNo(gateInTestDataReq.getTruckNo());
+					pickupHistory1.setChassisNo(gateInTestDataReq.getChassisNo());
+					pickupHistory1.setStatus(0);
+					pickupHistory1.setGatePass(gateInTestDataReq.getGatePass());
+					pickupHistoryService.insertPickupHistory(pickupHistory1);
+				}
+			}
+		}
+		
+		
 		return success();
 	}
 }
---
+++ eport-admin/src/main/java/vn/com/irtech/eport/web/dto/GateInTestDataReq.java
@@ -0,0 +1,179 @@
+package vn.com.irtech.eport.web.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+
+import com.google.firebase.database.annotations.NotNull;
+
+public class GateInTestDataReq  implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@NotBlank
+	private String truckNo;
+	
+	@NotBlank
+	private String chassisNo;
+	
+	@NotBlank
+	private String gatePass;
+	
+	@NotNull
+	private Integer weight;
+	
+	private String containerSend1;
+	
+	private String containerSend2;
+	
+	private String blNo;
+	
+	private String refNo;
+	
+	private String containerReceive1;
+	
+	private String containerReceive2;
+	
+    private Integer containerAmount;
+    
+    private String containerFlg;
+    
+    private Boolean sendOption;
+    
+    private Boolean receiveOption;
+    
+    @NotNull
+    private Long logisticGroupId;
+    
+    @NotNull
+    private Long driverId;
+
+	public String getTruckNo() {
+		return truckNo;
+	}
+
+	public void setTruckNo(String truckNo) {
+		this.truckNo = truckNo;
+	}
+
+	public String getChassisNo() {
+		return chassisNo;
+	}
+
+	public void setChassisNo(String chassisNo) {
+		this.chassisNo = chassisNo;
+	}
+
+	public String getGatePass() {
+		return gatePass;
+	}
+
+	public void setGatePass(String gatePass) {
+		this.gatePass = gatePass;
+	}
+
+	public Integer getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+
+	public String getContainerSend1() {
+		return containerSend1;
+	}
+
+	public void setContainerSend1(String containerSend1) {
+		this.containerSend1 = containerSend1;
+	}
+
+	public String getContainerSend2() {
+		return containerSend2;
+	}
+
+	public void setContainerSend2(String containerSend2) {
+		this.containerSend2 = containerSend2;
+	}
+
+	public String getBlNo() {
+		return blNo;
+	}
+
+	public void setBlNo(String blNo) {
+		this.blNo = blNo;
+	}
+
+	public String getRefNo() {
+		return refNo;
+	}
+
+	public void setRefNo(String refNo) {
+		this.refNo = refNo;
+	}
+
+	public String getContainerReceive1() {
+		return containerReceive1;
+	}
+
+	public void setContainerReceive1(String containerReceive1) {
+		this.containerReceive1 = containerReceive1;
+	}
+
+	public String getContainerReceive2() {
+		return containerReceive2;
+	}
+
+	public void setContainerReceive2(String containerReceive2) {
+		this.containerReceive2 = containerReceive2;
+	}
+
+	public Integer getContainerAmount() {
+		return containerAmount;
+	}
+
+	public void setContainerAmount(Integer containerAmount) {
+		this.containerAmount = containerAmount;
+	}
+
+	public Long getLogisticGroupId() {
+		return logisticGroupId;
+	}
+
+	public void setLogisticGroupId(Long logisticGroupId) {
+		this.logisticGroupId = logisticGroupId;
+	}
+
+	public Long getDriverId() {
+		return driverId;
+	}
+
+	public void setDriverId(Long driverId) {
+		this.driverId = driverId;
+	}
+
+	public String getContainerFlg() {
+		return containerFlg;
+	}
+
+	public void setContainerFlg(String containerFlg) {
+		this.containerFlg = containerFlg;
+	}
+
+	public Boolean getSendOption() {
+		return sendOption;
+	}
+
+	public void setSendOption(Boolean sendOption) {
+		this.sendOption = sendOption;
+	}
+
+	public Boolean getReceiveOption() {
+		return receiveOption;
+	}
+
+	public void setReceiveOption(Boolean receiveOption) {
+		this.receiveOption = receiveOption;
+	}
+	
+}
