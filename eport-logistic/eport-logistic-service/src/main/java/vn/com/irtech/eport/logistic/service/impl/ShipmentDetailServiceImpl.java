4f3181501deaf6c6ab702ca0785c274127fedc25
Trong Hieu <tronghieu8531@gmail.com>
2020-09-03 01:35:58 +0700

    - Fix add shipment need to attach booking

--- eport-logistic/eport-logistic-service/src/main/java/vn/com/irtech/eport/logistic/service/impl/ShipmentDetailServiceImpl.java
+++ eport-logistic/eport-logistic-service/src/main/java/vn/com/irtech/eport/logistic/service/impl/ShipmentDetailServiceImpl.java
@@ -628,7 +628,9 @@ public class ShipmentDetailServiceImpl implements IShipmentDetailService {
             shipmentDetail.setProcessOrderId(processOrder.getId());
             shipmentDetail.setRegisterNo(detail.getId().toString());
             shipmentDetail.setUserVerifyStatus("Y");
-            shipmentDetail.setOpeCode(shipment.getOpeCode());
+            if (processOrder.getServiceType() == EportConstants.SERVICE_DROP_FULL) {
+            	shipmentDetail.setOpeCode(shipment.getOpeCode());
+            }
             shipmentDetailMapper.updateShipmentDetail(shipmentDetail);
             if (processOrder.getServiceType() == 2) {
             	shipmentDetail.setRemark("Ha vo " + shipmentDetail.getEmptyDepotLocation());
