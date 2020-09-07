3568bb7e6ffa29b8e2852c6dcddb4f2d50b0247e
Trong Hieu <tronghieu8531@gmail.com>
2020-09-07 08:53:37 +0700

    - Update document gathering

--- eport-admin/src/main/java/vn/com/irtech/eport/web/controller/om/DocumentGatheringController.java
+++ eport-admin/src/main/java/vn/com/irtech/eport/web/controller/om/DocumentGatheringController.java
@@ -1,19 +1,135 @@
 package vn.com.irtech.eport.web.controller.om;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Date;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import vn.com.irtech.eport.common.constant.EportConstants;
+import vn.com.irtech.eport.common.core.domain.AjaxResult;
+import vn.com.irtech.eport.common.core.page.PageAble;
+import vn.com.irtech.eport.common.utils.StringUtils;
+import vn.com.irtech.eport.logistic.domain.LogisticGroup;
+import vn.com.irtech.eport.logistic.domain.Shipment;
+import vn.com.irtech.eport.logistic.domain.ShipmentComment;
+import vn.com.irtech.eport.logistic.domain.ShipmentDetail;
+import vn.com.irtech.eport.logistic.service.ICatosApiService;
+import vn.com.irtech.eport.logistic.service.ILogisticGroupService;
+import vn.com.irtech.eport.logistic.service.IShipmentCommentService;
+import vn.com.irtech.eport.logistic.service.IShipmentDetailService;
+import vn.com.irtech.eport.logistic.service.IShipmentService;
 import vn.com.irtech.eport.web.controller.AdminBaseController;
 
 @Controller
 @RequestMapping("/om/document")
 public class DocumentGatheringController extends AdminBaseController  {
+	
+	private static final Logger logger = LoggerFactory.getLogger(DocumentGatheringController.class); 
 
-	private String prefix = "om/document";
+	private String PREFIX = "om/document";
+	
+	@Autowired
+	private IShipmentService shipmentService;
+	
+	@Autowired
+	private ILogisticGroupService logisticGroupService;
+	
+	@Autowired
+	private ICatosApiService catosApiService;
+	
+	@Autowired
+	private IShipmentDetailService shipmentDetailService;
+	
+	@Autowired
+	private IShipmentCommentService shipmentCommentService;
 	
 	@GetMapping()
-	public String getViewexEcuteCatos() {
-		return prefix + "/index";
+	public String getViewDocument(ModelMap mmap) {
+		
+		// Get list logistic group
+		LogisticGroup logisticGroup = new LogisticGroup();
+	    logisticGroup.setGroupName("Chọn đơn vị Logistics");
+	    logisticGroup.setId(0L);
+	    LogisticGroup logisticGroupParam = new LogisticGroup();
+	    logisticGroupParam.setDelFlag("0");
+	    List<LogisticGroup> logisticGroups = logisticGroupService.selectLogisticGroupList(logisticGroupParam);
+	    logisticGroups.add(0, logisticGroup);
+	    mmap.put("logisticGroups", logisticGroups);
+	    
+	    // Get list vslNm : vslNmae : voyNo
+	    List<ShipmentDetail> berthplanList = catosApiService.selectVesselVoyageBerthPlanWithoutOpe();
+	    ShipmentDetail shipmentDetail = new ShipmentDetail();
+	    shipmentDetail.setVslAndVoy("Chọn tàu chuyến");
+	    berthplanList.add(0, shipmentDetail);
+	    mmap.put("vesselAndVoyages", berthplanList);
+		return PREFIX + "/index";
+	}
+	
+	@GetMapping("/confirmation")
+	public String getConfirmationForm() {
+		return PREFIX + "/confirmation";
+	}
+	
+	@PostMapping("/shipments")
+	@ResponseBody
+	public AjaxResult getShipments(@RequestBody PageAble<Shipment> param) {
+		startPage(param.getPageNum(), param.getPageSize(), param.getOrderBy());
+		AjaxResult ajaxResult = AjaxResult.success();
+		Shipment shipment = param.getData();
+		if (shipment == null) {
+			shipment = new Shipment();
+		}
+		shipment.setServiceType(EportConstants.SERVICE_PICKUP_FULL);
+		List<Shipment> shipments = shipmentService.selectShipmentListByWithShipmentDetailFilter(shipment);
+		ajaxResult.put("shipments", getDataTable(shipments));
+		return ajaxResult;
+	}
+	
+	@GetMapping("/shipment/{shipmentId}/shipmentDetails")
+	@ResponseBody
+	public AjaxResult getShipmentDetails(@PathVariable("shipmentId") Long shipmentId) {
+		AjaxResult ajaxResult = AjaxResult.success();
+		ShipmentDetail shipmentDetail = new ShipmentDetail();
+		shipmentDetail.setShipmentId(shipmentId);
+		List<ShipmentDetail> shipmentDetails = shipmentDetailService.selectShipmentDetailList(shipmentDetail);
+		ajaxResult.put("shipmentDetails", shipmentDetails);
+		return ajaxResult;
+	}
+	
+	@PostMapping("/confirmation")
+	@ResponseBody
+	@Transactional
+	public AjaxResult submitConfirmation(String doStatus, String content, String shipmentDetailIds) {
+		List<ShipmentDetail> shipmentDetails = shipmentDetailService.selectShipmentDetailByIds(shipmentDetailIds, null);
+		for (ShipmentDetail shipmentDetail : shipmentDetails) {
+			shipmentDetail.setDoStatus(doStatus);
+			shipmentDetail.setUpdateBy(getUser().getUserName());
+			shipmentDetailService.updateShipmentDetail(shipmentDetail);
+		}
+		if (StringUtils.isNotEmpty(content)) {
+			ShipmentDetail shipmentDetail = shipmentDetails.get(0);
+			ShipmentComment shipmentComment = new ShipmentComment();
+			shipmentComment.setLogisticGroupId(shipmentDetail.getLogisticGroupId());
+			shipmentComment.setShipmentId(shipmentDetail.getShipmentId());
+			shipmentComment.setUserId(getUserId());
+			shipmentComment.setUserType(EportConstants.COMMENTOR_DNP_STAFF);
+			shipmentComment.setUserAlias(getUser().getDept().getDeptName());
+			shipmentComment.setCommentTime(new Date());
+			shipmentComment.setContent(content);
+			
+			shipmentCommentService.updateShipmentComment(shipmentComment);
+		}
+ 		return success("Thu chứng từ gốc thành công");
 	}
 }
