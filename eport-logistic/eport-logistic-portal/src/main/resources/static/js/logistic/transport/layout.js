c3d82d5febfcce305c4ac1dd2a7d7facafb1f7a5
Anh Tài <nqat2003@gmail.com>
2020-09-21 01:31:10 +0700

    - Update view driver manager (transport index)

--- eport-logistic/eport-logistic-portal/src/main/resources/static/js/logistic/transport/layout.js
+++ eport-logistic/eport-logistic-portal/src/main/resources/static/js/logistic/transport/layout.js
@@ -18,19 +18,3 @@ $(".uncollapse").click(function () {
   currentHeight = $(document).innerHeight() - 50;
   loadTable();
 });
-
-function dateformatter(date) {
-  var y = date.getFullYear();
-  var m = date.getMonth() + 1;
-  var d = date.getDate();
-  return (d < 10 ? "0" + d : d) + "/" + (m < 10 ? "0" + m : m) + "/" + y;
-}
-function dateparser(s) {
-  var ss = s.split(".");
-  var d = parseInt(ss[0], 10);
-  var m = parseInt(ss[1], 10);
-  var y = parseInt(ss[2], 10);
-  if (!isNaN(y) && !isNaN(m) && !isNaN(d)) {
-    return new Date(y, m - 1, d);
-  }
-}
