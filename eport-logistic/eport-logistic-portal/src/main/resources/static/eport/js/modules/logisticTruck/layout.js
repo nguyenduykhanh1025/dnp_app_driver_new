b98d194d360f21868f211136940d7b1076c22699
Anh Tài <nqat2003@gmail.com>
2020-09-21 01:50:29 +0700

    - Update view truck history

---
+++ eport-logistic/eport-logistic-portal/src/main/resources/static/eport/js/modules/logisticTruck/layout.js
@@ -0,0 +1,37 @@
+var currentHeight = $(document).innerHeight() - 50;
+$(".main-body").layout();
+
+$(".collapse").click(function () {
+  $(".main-body__search-wrapper").hide();
+  $(".main-body__search-wrapper--container").hide();
+  $(this).hide();
+  $(".uncollapse").show();
+  currentHeight = $(document).innerHeight() - 10;
+  loadTable();
+});
+
+$(".uncollapse").click(function () {
+  $(".main-body__search-wrapper").show();
+  $(".main-body__search-wrapper--container").show();
+  $(this).hide();
+  $(".collapse").show();
+  currentHeight = $(document).innerHeight() - 50;
+  loadTable();
+});
+
+
+function dateformatter(date) {
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+    return (d < 10 ? "0" + d : d) + "/" + (m < 10 ? "0" + m : m) + "/" + y;
+  }
+  function dateparser(s) {
+    var ss = s.split(".");
+    var d = parseInt(ss[0], 10);
+    var m = parseInt(ss[1], 10);
+    var y = parseInt(ss[2], 10);
+    if (!isNaN(y) && !isNaN(m) && !isNaN(d)) {
+      return new Date(y, m - 1, d);
+    }
+  }
