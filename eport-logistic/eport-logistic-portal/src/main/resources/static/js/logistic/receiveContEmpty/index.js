230a2f9e3a43a110ec43ad55989bcdd16e541a40
Trong Hieu <tronghieu8531@gmail.com>
2020-09-28 09:59:37 +0700

    - Reduce height form confirm before verify otp

--- eport-logistic/eport-logistic-portal/src/main/resources/static/js/logistic/receiveContEmpty/index.js
+++ eport-logistic/eport-logistic-portal/src/main/resources/static/js/logistic/receiveContEmpty/index.js
@@ -1502,7 +1502,7 @@ function requestCont(index, layero) {
 function verify() {
     getDataSelectedFromTable(true);
     if (shipmentDetails.length > 0) {
-        $.modal.openCustomForm("Xác nhận làm lệnh", prefix + "/otp/cont-list/confirmation/" + shipmentDetailIds, 700, 600);
+        $.modal.openCustomForm("Xác nhận làm lệnh", prefix + "/otp/cont-list/confirmation/" + shipmentDetailIds, 700, 500);
     }
 }
 
