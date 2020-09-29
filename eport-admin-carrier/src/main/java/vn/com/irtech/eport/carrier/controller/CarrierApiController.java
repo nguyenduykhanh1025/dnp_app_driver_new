0bae9b1a0b53b636cf46b3c32b4be76e7736c631
Trong Hieu <tronghieu8531@gmail.com>
2020-09-29 08:40:11 +0700

    + Add screen carrier api admin

---
+++ eport-admin-carrier/src/main/java/vn/com/irtech/eport/carrier/controller/CarrierApiController.java
@@ -0,0 +1,128 @@
+package vn.com.irtech.eport.carrier.controller;
+
+import java.util.List;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import vn.com.irtech.eport.carrier.domain.CarrierApi;
+import vn.com.irtech.eport.carrier.service.ICarrierApiService;
+import vn.com.irtech.eport.common.annotation.Log;
+import vn.com.irtech.eport.common.core.controller.BaseController;
+import vn.com.irtech.eport.common.core.domain.AjaxResult;
+import vn.com.irtech.eport.common.core.page.TableDataInfo;
+import vn.com.irtech.eport.common.enums.BusinessType;
+import vn.com.irtech.eport.common.utils.poi.ExcelUtil;
+
+/**
+ *  Carrier api Controller
+ * 
+ * @author Trong Hieu
+ * @date 2020-09-28
+ */
+@Controller
+@RequestMapping("/carrier/api")
+public class CarrierApiController extends BaseController
+{
+    private String prefix = "carrier/api";
+
+    @Autowired
+    private ICarrierApiService carrierApiService;
+
+    @RequiresPermissions("carrier:api:view")
+    @GetMapping()
+    public String api()
+    {
+        return prefix + "/api";
+    }
+
+    /**
+     * Get carrier api List
+     */
+    @RequiresPermissions("carrier:api:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(CarrierApi carrierApi)
+    {
+        startPage();
+        List<CarrierApi> list = carrierApiService.selectCarrierApiList(carrierApi);
+        return getDataTable(list);
+    }
+
+    /**
+     * Export carrier api List
+     */
+    @RequiresPermissions("carrier:api:export")
+    @Log(title = "Export carrier api list", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(CarrierApi carrierApi)
+    {
+        List<CarrierApi> list = carrierApiService.selectCarrierApiList(carrierApi);
+        ExcelUtil<CarrierApi> util = new ExcelUtil<CarrierApi>(CarrierApi.class);
+        return util.exportExcel(list, "api");
+    }
+
+    /**
+     * Add carrier api
+     */
+    @GetMapping("/add")
+    public String add()
+    {
+        return prefix + "/add";
+    }
+
+    /**
+     * Add carrier api
+     */
+    @RequiresPermissions("carrier:api:add")
+    @Log(title = "Add carrier api", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(CarrierApi carrierApi)
+    {
+        return toAjax(carrierApiService.insertCarrierApi(carrierApi));
+    }
+
+    /**
+     * Update Carrier api
+     */
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, ModelMap mmap)
+    {
+        CarrierApi carrierApi = carrierApiService.selectCarrierApiById(id);
+        mmap.put("carrierApi", carrierApi);
+        return prefix + "/edit";
+    }
+
+    /**
+     * Update Save Carrier Api
+     */
+    @RequiresPermissions("carrier:api:edit")
+    @Log(title = "Update save carrier api", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(CarrierApi carrierApi)
+    {
+        return toAjax(carrierApiService.updateCarrierApi(carrierApi));
+    }
+
+    /**
+     * Delete Carrier Api
+     */
+    @RequiresPermissions("carrier:api:remove")
+    @Log(title = "Delete carrier api", businessType = BusinessType.DELETE)
+    @PostMapping( "/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids)
+    {
+        return toAjax(carrierApiService.deleteCarrierApiByIds(ids));
+    }
+}
---
+++ eport-carrier-service/src/main/java/vn/com/irtech/eport/carrier/mapper/CarrierApiMapper.java
@@ -0,0 +1,61 @@
+package vn.com.irtech.eport.carrier.mapper;
+
+import java.util.List;
+
+import vn.com.irtech.eport.carrier.domain.CarrierApi;
+
+/**
+ * 
+ * @author Trong Hieu
+ * @date 2020-09-28
+ */
+public interface CarrierApiMapper 
+{
+    /**
+     * Get carrier api
+     * 
+     * @param id carrier api ID
+     * @return carrier api
+     */
+    public CarrierApi selectCarrierApiById(Long id);
+
+    /**
+     * Get carrier api List
+     * 
+     * @param carrierApi carrier api
+     * @return carrier api List
+     */
+    public List<CarrierApi> selectCarrierApiList(CarrierApi carrierApi);
+
+    /**
+     * Add carrier api
+     * 
+     * @param carrierApi carrier api
+     * @return Result
+     */
+    public int insertCarrierApi(CarrierApi carrierApi);
+
+    /**
+     * Update carrier api
+     * 
+     * @param carrierApi carrier api
+     * @return Result
+     */
+    public int updateCarrierApi(CarrierApi carrierApi);
+
+    /**
+     * Delete carrier api
+     * 
+     * @param id carrier api ID
+     * @return result
+     */
+    public int deleteCarrierApiById(Long id);
+
+    /**
+     * Batch Delete carrier api
+     * 
+     * @param ids IDs
+     * @return result
+     */
+    public int deleteCarrierApiByIds(String[] ids);
+}
