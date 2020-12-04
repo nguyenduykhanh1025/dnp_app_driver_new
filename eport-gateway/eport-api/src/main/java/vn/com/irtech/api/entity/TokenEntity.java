99c3840cd0aadad8bdb64c39e57134cd1c7eb7ac
VietNhat <lenhat96qt@gmail.com>
2020-12-04 11:47:02 +0700

    -Commit fix pussh thừa entity get way

--- eport-gateway/eport-api/src/main/java/vn/com/irtech/api/entity/TokenEntity.java
+++ eport-gateway/eport-api/src/main/java/vn/com/irtech/api/entity/TokenEntity.java
@@ -22,37 +22,4 @@ public class TokenEntity implements Serializable {
 
 	private Date updateTime;
 
-	public Long getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-
-	public String getToken() {
-		return token;
-	}
-
-	public void setToken(String token) {
-		this.token = token;
-	}
-
-	public Date getExpireTime() {
-		return expireTime;
-	}
-
-	public void setExpireTime(Date expireTime) {
-		this.expireTime = expireTime;
-	}
-
-	public Date getUpdateTime() {
-		return updateTime;
-	}
-
-	public void setUpdateTime(Date updateTime) {
-		this.updateTime = updateTime;
-	}
-	
-
 }
