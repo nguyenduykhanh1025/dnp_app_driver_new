afab11837de940e041334a9ba6ef9fddd66c5486
Trong Hieu <tronghieu8531@gmail.com>
2020-12-19 12:08:56 +0700

    - Update

--- eport-logistic/eport-logistic-portal/src/main/java/vn/com/irtech/eport/logistic/listener/RobotUpdateStatusHandler.java
+++ eport-logistic/eport-logistic-portal/src/main/java/vn/com/irtech/eport/logistic/listener/RobotUpdateStatusHandler.java
@@ -200,6 +200,9 @@ public class RobotUpdateStatusHandler implements IMqttMessageListener {
 		if (isExtensionDetOrder) {
 			serviceTypes += EportConstants.SERVICE_EXTEND_DET + ",";
 		}
+		if (isOverSizeRemarkOrder) {
+			serviceTypes += EportConstants.SERVICE_OVERSIZE_REMARK_ORDER + ",";
+		}
 		
 		if (serviceTypes.length() > 0) {
 			serviceTypes = serviceTypes.substring(0, serviceTypes.length()-1);
@@ -226,6 +229,7 @@ public class RobotUpdateStatusHandler implements IMqttMessageListener {
 		sysRobot.setIsCancelReceiveContEmptyOrder(isCancelReceiveContEmptyOrder);
 		sysRobot.setIsExportReceipt(isExportReceipt);
 		sysRobot.setIsExtensionDetOrder(isExtensionDetOrder);
+		sysRobot.(isExtensionDetOrder);
 
 		// check if robot is exists but be disabled then just update robot infor but not validate or assign new order to robot
 		SysRobot robotExist = robotService.selectRobotByUuId(uuId);
