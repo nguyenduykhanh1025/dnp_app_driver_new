3291d8404ecc7ea165c38f4e67860bafe322c337
Trong Hieu <tronghieu8531@gmail.com>
2020-12-25 14:18:39 +0700

    - Update bulletin in carrier

---
+++ eport-carrier-portal/src/main/java/vn/com/irtech/eport/carrier/controller/CarrierBulletinController.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package vn.com.irtech.eport.carrier.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import vn.com.irtech.eport.common.constant.EportConstants;
+import vn.com.irtech.eport.common.core.domain.AjaxResult;
+import vn.com.irtech.eport.common.core.page.PageAble;
+import vn.com.irtech.eport.common.core.page.TableDataInfo;
+import vn.com.irtech.eport.system.domain.SysNotice;
+import vn.com.irtech.eport.system.service.ISysNoticeService;
+
+/**
+ * @author Trong Hieu
+ *
+ */
+@Controller
+@RequestMapping("/carrier/bulletin")
+public class CarrierBulletinController extends CarrierBaseController {
+
+	@Autowired
+	private ISysNoticeService sysNoticeService;
+
+	@PostMapping("/list")
+	@ResponseBody
+	public AjaxResult getBulletinList(@RequestBody PageAble<SysNotice> params) {
+		startPage(params.getPageNum(), params.getPageSize(), params.getOrderBy());
+		SysNotice sysNoticeParam = params.getData();
+		if (sysNoticeParam == null) {
+			sysNoticeParam = new SysNotice();
+		}
+		sysNoticeParam.setActive(EportConstants.BULLETIN_RELEASE);
+		List<SysNotice> sysNotices = sysNoticeService.selectBulletinList(sysNoticeParam);
+		if (CollectionUtils.isEmpty(sysNotices)) {
+			sysNotices = new ArrayList<>();
+		}
+		TableDataInfo tableDataInfo = getDataTable(sysNotices);
+		AjaxResult ajaxResult = AjaxResult.success();
+		ajaxResult.put("bulletins", tableDataInfo);
+		return ajaxResult;
+	}
+
+}
